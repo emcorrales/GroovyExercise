823f037106fd9cf34c0b84392edffd6955cb84a0
Emmanuel Corrales <emc@Terra>
2024-07-10 11:56:56 +0800

    Check if the user entered the right arguments and display instructions on how to properly use the program as a command line.

--- src/main/groovy/com/emcorrales/Main.groovy
+++ src/main/groovy/com/emcorrales/Main.groovy
@@ -1,5 +1,18 @@
 package com.emcorrales
 
 static void main(String[] args) {
+    // End program with a message if arguments are invalid.
+    if (args.length<3) {
+        println "Invalid arguments. Please enter the following required arguments:"
+        println "1. A path to a directory containing some text file (you can assume that the \n" +
+                "directory contains only text files) "
+        println "2. The original text or pattern to use for searching in the files (in the given directory \n" +
+                "or its subdirectories). "
+        println "3. A new text string which will replace the original text or pattern if found in the file \n" +
+                "(as many times as it was found). "
+        println "4. The class will also allow another optional argument for a path to a file for outputting a \n" +
+                "list of which files were modified (logging)."
+        return
+    }
     println "Hello world!"
 }
