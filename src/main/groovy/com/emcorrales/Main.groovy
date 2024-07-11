7e4212f6c6cbf9b29269ef4a486a285715f65311
Emmanuel Corrales <emc@Terra>
2024-07-11 08:51:09 +0800

    Change the messages for arguments are invalid.

--- src/main/groovy/com/emcorrales/Main.groovy
+++ src/main/groovy/com/emcorrales/Main.groovy
@@ -10,14 +10,10 @@ class GroovyExercise {
         // End program with a message if arguments are invalid.
         if (args.length < 3) {
             println "Invalid arguments. Please enter the following required arguments:"
-            println "1. A path to a directory containing some text file (you can assume that the \n" +
-                    "directory contains only text files) "
-            println "2. The original text or pattern to use for searching in the files (in the given directory \n" +
-                    "or its subdirectories). "
-            println "3. A new text string which will replace the original text or pattern if found in the file \n" +
-                    "(as many times as it was found). "
-            println "4. The class will also allow another optional argument for a path to a file for outputting a \n" +
-                    "list of which files were modified (logging)."
+            println "1. REQUIRED A path to a directory containing some text file."
+            println "2. REQUIRED The original text to be replaced."
+            println "3. REQUIRED The text that will replace the original"
+            println "4. OPTIONAL Path to a file for logging."
             return
         }
 
